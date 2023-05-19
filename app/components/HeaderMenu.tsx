765c626ace50c2aebb0025aed2554cb4a96b7a75
emanuelefavero <emafaux89@gmail.com>
2023-05-19 10:40:11 +0200

    add gradient text to contact me link and line animation to others

--- app/components/HeaderMenu.tsx
+++ app/components/HeaderMenu.tsx
@@ -1,16 +1,31 @@
+import styles from './HeaderMenu.module.scss'
 import Link from 'next/link'
 
 export default function HeaderMenu() {
   return (
     <ul className='mr-4 flex items-center justify-center text-xl font-semibold text-slate-700'>
       <li className='mr-6'>
-        <Link href='#portfolio'>Portfolio</Link>
+        <Link
+          href='#portfolio'
+          className={`${styles.linkHoverLineAnimation} relative active:text-slate-600`}
+        >
+          Portfolio
+        </Link>
       </li>
       <li className='mr-6'>
-        <Link href='#skills'>Skills</Link>
+        <Link
+          href='#skills'
+          className={`${styles.linkHoverLineAnimation} relative active:text-slate-600`}
+        >
+          Skills
+        </Link>
       </li>
       <li className='mr-6'>
-        <Link href='/emanuele-favero-CV.pdf' target='_blank'>
+        <Link
+          href='/emanuele-favero-CV.pdf'
+          target='_blank'
+          className={`${styles.linkHoverLineAnimation} relative active:text-slate-600`}
+        >
           Download CV
         </Link>
       </li>
@@ -19,7 +34,12 @@ export default function HeaderMenu() {
           href='#contact-me'
           className='relative transform rounded-full border-b border-slate-300 bg-slate-50 px-4 py-2 shadow-sm shadow-slate-400 transition-all duration-100 hover:bg-slate-100 hover:text-slate-600 active:top-[1px] active:shadow-none'
         >
-          Contact Me
+          <span
+            className='bg-gradient-to-r from-teal-700 to-teal-500 bg-clip-text text-transparent
+          '
+          >
+            Contact Me
+          </span>
         </Link>
       </li>
     </ul>
