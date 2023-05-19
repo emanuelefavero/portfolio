2c529302f0bc4aa13a9e592fea40325e7b60591a
emanuelefavero <emafaux89@gmail.com>
2023-05-19 04:57:38 +0200

    add barebone Header component

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import './globals.css'
+import Header from './components/Header'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -16,7 +17,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang='en'>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <header>
+          <Header />
+        </header>
+
+        <main>{children}</main>
+      </body>
     </html>
   )
 }
