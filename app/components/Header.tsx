4e810fe662733a61495871a378ad7c13a659d2ff
emanuelefavero <emafaux89@gmail.com>
2023-05-19 05:33:22 +0200

    add logo (image and text)

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,3 +1,19 @@
+import Image from 'next/image'
+
 export default function Header() {
-  return <div>Header</div>
+  return (
+    <div className='flex w-full items-center justify-between bg-slate-200 px-3 py-3'>
+      <div className='flex items-center justify-center'>
+        <Image
+          className='mr-4 rounded-full border-2 border-slate-100 shadow-sm shadow-slate-400'
+          src='/images/logo-400x400.jpeg'
+          alt='logo'
+          width={44}
+          height={44}
+        />
+        <span className='text-xl font-semibold'>Emanuele Favero</span>
+      </div>
+      <ul>Hey</ul>
+    </div>
+  )
 }
