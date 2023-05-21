287699d206d169a63ead3933e374f6929e7e8f71
emanuelefavero <emafaux89@gmail.com>
2023-05-21 04:32:35 +0200

    add hover and active state for HeroButton

--- app/components/HeroButton.tsx
+++ app/components/HeroButton.tsx
@@ -1,9 +1,10 @@
+import styles from './HeroButton.module.scss'
+
 export default function HeroButton() {
   return (
     <>
       <button
-        className='mb-36 rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white'
-        type='button'
+        className={`${styles.HeroButton} hover:tracking-wide' type='button mb-36 rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white`}
       >
         Let&apos;s Talk
       </button>
