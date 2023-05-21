d3c2b451fcbc36a202636b656c34f21efefdbe01
emanuelefavero <emafaux89@gmail.com>
2023-05-21 05:22:26 +0200

    add shadow to HeroButton

--- app/components/HeroButton.tsx
+++ app/components/HeroButton.tsx
@@ -5,7 +5,7 @@ export default function HeroButton() {
     <>
       <button
         type='button'
-        className={`${styles.HeroButton} hover:tracking-wide' mb-36 select-none rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white`}
+        className={`${styles.HeroButton} hover:tracking-wide' mb-36 select-none rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white shadow-sm shadow-indigo-300`}
       >
         Let&apos;s Talk
       </button>
