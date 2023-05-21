5fb4a694e02395878508b28d65d11afff7ecb187
emanuelefavero <emafaux89@gmail.com>
2023-05-21 03:49:01 +0200

    add hover and active state to HeroSocialLinks

--- app/components/HeroSocialLinks.tsx
+++ app/components/HeroSocialLinks.tsx
@@ -10,28 +10,28 @@ export default function HeroSocialLinks() {
       <nav className='flex items-center justify-start'>
         <Link href='mailto:info@emanuelefavero.com' target='_blank'>
           <MdEmail
-            className='mr-5 text-2xl text-slate-700'
+            className='mr-5 transform text-2xl text-slate-700 transition-all duration-200 hover:rotate-12 hover:scale-125 hover:text-slate-600 active:scale-110 active:text-indigo-600'
             aria-label='info@emanuelefavero.com'
             title='info@emanuelefavero.com'
           />
         </Link>
         <Link href='https://www.linkedin.com/in/emanuelefavero' target='_blank'>
           <BsLinkedin
-            className='mr-5 text-xl text-slate-700'
+            className='mr-5 transform text-xl text-slate-700 transition-all duration-200 hover:rotate-12 hover:scale-125 hover:text-slate-600 active:scale-110 active:text-indigo-600'
             aria-label='linkedin.com/in/emanuelefavero'
             title='linkedin.com/in/emanuelefavero'
           />
         </Link>
         <Link href='https://github.com/emanuelefavero' target='_blank'>
           <BsGithub
-            className='mr-5 text-xl text-slate-700'
+            className='mr-5 transform text-xl text-slate-700 transition-all duration-200 hover:rotate-12 hover:scale-125 hover:text-slate-600 active:scale-110 active:text-indigo-600'
             aria-label='github.com/emanuelefavero'
             title='github.com/emanuelefavero'
           />
         </Link>
         <Link href='https://t.me/emanuelefavero' target='_blank'>
           <BsTelegram
-            className='mr-5 text-xl text-slate-700'
+            className='mr-5 transform text-xl text-slate-700 transition-all duration-200 hover:rotate-12 hover:scale-125 hover:text-slate-600 active:scale-110 active:text-indigo-600'
             aria-label='t.me/emanuelefavero'
             title='t.me/emanuelefavero'
           />
