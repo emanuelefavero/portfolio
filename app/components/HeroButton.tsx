6adfff4ce41fa43cc53a5058a6631f900c9b5e8e
emanuelefavero <emafaux89@gmail.com>
2023-05-21 05:18:29 +0200

    fix type=button in HeroButton

--- app/components/HeroButton.tsx
+++ app/components/HeroButton.tsx
@@ -4,10 +4,13 @@ export default function HeroButton() {
   return (
     <>
       <button
-        className={`${styles.HeroButton} hover:tracking-wide' type='button mb-36 select-none rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white`}
+        type='button'
+        className={`${styles.HeroButton} hover:tracking-wide' mb-36 select-none rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white`}
       >
         Let&apos;s Talk
       </button>
     </>
   )
 }
+
+// shadow-sm shadow-slate-400
