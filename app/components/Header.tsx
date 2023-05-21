2fe545e741a53d70c820b19e67008fefeaf82eac
emanuelefavero <emafaux89@gmail.com>
2023-05-21 12:23:33 +0200

    change HeaderLogo behavior - go to #home instead of refresh

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,7 +3,11 @@ import HeaderMenu from './HeaderMenu'
 
 export default function Header() {
   return (
-    <div className='flex w-full items-center justify-between bg-slate-200 px-2 py-3'>
+    <div
+      // TODO:Move this id to the hero section if you change the header to be fixed
+      id='home'
+      className='flex w-full items-center justify-between bg-slate-200 px-2 py-3'
+    >
       <HeaderLogo />
       <HeaderMenu />
     </div>
--- app/components/HeaderLogo.tsx
+++ app/components/HeaderLogo.tsx
@@ -1,17 +1,12 @@
-'use client'
-
 import styles from './HeaderLogo.module.scss'
+import Link from 'next/link'
 import Image from 'next/image'
-import { useRouter } from 'next/navigation'
 
 export default function HeaderLogo() {
-  const router = useRouter()
-
   return (
-    <button
-      type='button'
+    <Link
+      href='/#home'
       className={`${styles.HeaderLogo} flex cursor-pointer items-center justify-center rounded-lg px-2 py-1 text-xl font-bold text-slate-900`}
-      onClick={() => router.push('/')}
     >
       <Image
         className='mr-4 select-none rounded-full border-2 border-slate-100 shadow-sm shadow-slate-400'
@@ -21,6 +16,6 @@ export default function HeaderLogo() {
         height={44}
       />
       <span>Emanuele Favero</span>
-    </button>
+    </Link>
   )
 }
