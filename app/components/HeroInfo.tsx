56b282903816bf6ca85d1d8981e462755e6e1835
emanuelefavero <emafaux89@gmail.com>
2023-05-23 19:53:41 +0200

    center and change style of hero info

--- app/components/HeroInfo.tsx
+++ app/components/HeroInfo.tsx
@@ -5,21 +5,29 @@ interface Props {
 export default function HeroInfo({ numberOfRepositories }: Props) {
   return (
     <>
-      <div className='absolute -right-16 top-10 text-xl font-medium text-slate-700'>
-        <div className='mb-6 w-max rounded-full border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-7 py-1'>
-          <p className='max-w-[6.3rem]'>
-            <span className='text-3xl font-bold'>
-              {/* Calculate years of experience (since 2018) */}
-              {new Date().getFullYear() - 2018}+
-            </span>{' '}
-            Years Experience
+      <div className='absolute -right-8 top-10 text-xl font-medium text-slate-700'>
+        <div className='mb-6 w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1'>
+          <p className='flex flex-col items-start justify-start'>
+            <div>
+              <span className='text-3xl font-bold'>
+                {/* Calculate years of experience (since 2018) */}
+                {new Date().getFullYear() - 2018}+
+              </span>{' '}
+              Years
+            </div>
+            Experience
           </p>
         </div>
-        <div className='w-max rounded-full border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-7 py-1'>
-          <p className='max-w-[9.3rem]'>
-            {/* Number of repositories */}
-            <span className='text-3xl font-bold'>{numberOfRepositories}</span>{' '}
-            GitHub Repositories
+        <div className='w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1'>
+          <p className='flex flex-col items-start justify-start'>
+            <div>
+              {/* Number of repositories */}
+              <span className='text-3xl font-bold'>
+                {numberOfRepositories}
+              </span>{' '}
+              GitHub
+            </div>
+            Repositories
           </p>
         </div>
       </div>
