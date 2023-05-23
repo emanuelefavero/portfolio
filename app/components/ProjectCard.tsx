8c94caa55d28f8c24ea192bf54b9dd2a7a37a267
emanuelefavero <emafaux89@gmail.com>
2023-05-23 18:16:47 +0200

    add websiteURL hover state in project card

--- app/components/ProjectCard.tsx
+++ app/components/ProjectCard.tsx
@@ -1,3 +1,4 @@
+import styles from './ProjectCard.module.scss'
 import Link from 'next/link'
 import Image from 'next/image'
 import { BsGithub } from 'react-icons/bs'
@@ -18,31 +19,34 @@ export default function ProjectCard({ project }: Props) {
   return (
     <>
       <div className='h-full w-80 flex-1 rounded-3xl border border-[#0a0c29] bg-[#171b56] shadow-sm shadow-[#0a0c29] transition-all duration-200 hover:scale-[1.02] hover:bg-[#1b2061] active:scale-[0.98]'>
-        <Link
-          // .websiteURL
-          href={project.websiteURL}
-          target='_blank'
-        >
-          <Image
-            className='mb-4 h-44 w-full rounded-t-3xl bg-slate-300'
-            // .image
-            src={project.image}
-            alt={project.name}
-            width={320}
-            height={176}
-          />
-          <div className='px-4'>
-            <h4 className='mb-2 text-xl font-semibold'>
-              {/* TODO: Replace emoji arrow with react icon and animate on hover */}
-              {/* .name */}
-              {project.name} ↗
-            </h4>
-            <p className='text-md mb-4 min-h-[5rem] font-medium text-[#a5b0d4]'>
-              {/* .description */}
-              {project.description}
-            </p>
-          </div>
-        </Link>
+        <div className={styles.websiteURL}>
+          <Link
+            // .websiteURL
+            href={project.websiteURL}
+            target='_blank'
+          >
+            <Image
+              className='mb-4 h-44 w-full rounded-t-3xl bg-slate-300'
+              // .image
+              src={project.image}
+              alt={project.name}
+              width={320}
+              height={176}
+            />
+            <div className='px-4'>
+              <h4 className='mb-2 text-xl font-semibold'>
+                {/* TODO: Replace emoji arrow with react icon and animate on hover */}
+                {/* .name */}
+                <span className={styles.projectName}>{project.name}</span>{' '}
+                <span className={styles.arrow}>↗</span>
+              </h4>
+              <p className='text-md mb-4 min-h-[5rem] font-medium text-[#a5b0d4]'>
+                {/* .description */}
+                {project.description}
+              </p>
+            </div>
+          </Link>
+        </div>
 
         <Link
           className='pb-4'
