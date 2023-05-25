bdf5b2052880715226c8a9e9ee9557696acbd1e0
emanuelefavero <emafaux89@gmail.com>
2023-05-25 10:35:51 +0200

    remove console.log and TODO from Projects.tsx

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,11 +1,7 @@
 import ProjectCard from './ProjectCard'
 import projectsData from '@/data/projects.json'
 
-// TODO: Add dynamic data from json file
-
 export default function Projects() {
-  console.log(projectsData.projects)
-
   return (
     <>
       <div className='flex w-full flex-row flex-wrap justify-center gap-8 pt-16'>
