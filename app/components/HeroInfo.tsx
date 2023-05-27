619fcbb326c67248296b7920503e5fe069827d44
emanuelefavero <emafaux89@gmail.com>
2023-05-27 21:27:23 +0200

    create fade in animation

--- app/components/HeroInfo.tsx
+++ app/components/HeroInfo.tsx
@@ -1,3 +1,5 @@
+import animations from '@/app/animations/fadeInAnimation.module.scss'
+
 interface Props {
   numberOfRepositories: number
 }
@@ -6,7 +8,9 @@ export default function HeroInfo({ numberOfRepositories }: Props) {
   return (
     <>
       <div className='absolute -right-8 top-10 text-xl font-medium text-slate-700'>
-        <div className='mb-6 w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1'>
+        <div
+          className={`${animations.fadeInAnimation} mb-6 w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1`}
+        >
           <div className='flex flex-col items-start justify-start'>
             <div>
               <span className='text-3xl font-bold'>
@@ -18,7 +22,10 @@ export default function HeroInfo({ numberOfRepositories }: Props) {
             Experience
           </div>
         </div>
-        <div className='w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1'>
+
+        <div
+          className={`${animations.fadeInAnimationDelayed} w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1`}
+        >
           <div className='flex flex-col items-start justify-start'>
             <div>
               {/* Number of repositories */}
