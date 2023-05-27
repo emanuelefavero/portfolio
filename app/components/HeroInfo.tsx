de13b68b5aab02f38f8b7acf36fb8926b5357d50
emanuelefavero <emafaux89@gmail.com>
2023-05-27 22:42:27 +0200

    remove fade in animation in Hero Info

--- app/components/HeroInfo.tsx
+++ app/components/HeroInfo.tsx
@@ -1,5 +1,3 @@
-import animations from '@/app/animations/fadeInAnimation.module.scss'
-
 interface Props {
   numberOfRepositories: number
 }
@@ -8,9 +6,7 @@ export default function HeroInfo({ numberOfRepositories }: Props) {
   return (
     <>
       <div className='absolute -right-8 top-10 text-xl font-medium text-slate-700'>
-        <div
-          className={`${animations.fadeInAnimation} mb-6 w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1`}
-        >
+        <div className='mb-6 w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1'>
           <div className='flex flex-col items-start justify-start'>
             <div>
               <span className='text-3xl font-bold'>
@@ -23,9 +19,7 @@ export default function HeroInfo({ numberOfRepositories }: Props) {
           </div>
         </div>
 
-        <div
-          className={`${animations.fadeInAnimationDelayed} w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1`}
-        >
+        <div className='w-max rounded-3xl border-b-2 border-slate-300 bg-slate-300 bg-opacity-30 px-4 py-1'>
           <div className='flex flex-col items-start justify-start'>
             <div>
               {/* Number of repositories */}
