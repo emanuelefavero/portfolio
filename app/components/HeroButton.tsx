29f3f1f9ae965a3b3abc3e1b8b31606e2d8a4b37
emanuelefavero <emafaux89@gmail.com>
2023-05-27 20:59:43 +0200

    assign #contact-me section to lets talk hero button

--- app/components/HeroButton.tsx
+++ app/components/HeroButton.tsx
@@ -1,14 +1,16 @@
 import styles from './HeroButton.module.scss'
+import Link from 'next/link'
 
 export default function HeroButton() {
   return (
     <>
-      <button
+      <Link
+        href='/#contact-me'
         type='button'
-        className={`${styles.HeroButton} hover:tracking-wide' mb-36 select-none rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white shadow-sm shadow-indigo-300`}
+        className={`${styles.HeroButton} hover:tracking-wide' mb-36 max-w-max select-none rounded-full bg-slate-900 px-6 py-2 text-xl font-bold text-white shadow-sm shadow-indigo-300`}
       >
         Let&apos;s Talk
-      </button>
+      </Link>
     </>
   )
 }
