0f295741ec9eaba91e887ef395b3267c71183160
emanuelefavero <emafaux89@gmail.com>
2023-05-27 22:22:23 +0200

    add FadeIntoView animation component

---
+++ app/animations/FadeIntoView.tsx
@@ -0,0 +1,40 @@
+'use client'
+
+import { useState, useRef, useEffect } from 'react'
+import styles from './FadeIntoView.module.scss'
+
+function FadeIntoView({ children }: { children: React.ReactNode }) {
+  const [isVisible, setVisible] = useState(true)
+  const [alreadyViewed, setAlreadyViewed] = useState(false)
+  const domRef = useRef() as React.MutableRefObject<HTMLInputElement>
+
+  useEffect(() => {
+    const currentDomRef = domRef.current
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        setVisible(entry.isIntersecting)
+
+        // NOTE: Stop the animation from playing when the element already appeared
+        if (entry.isIntersecting) {
+          setAlreadyViewed(true)
+        }
+      })
+    })
+    observer.observe(currentDomRef)
+
+    return () => observer.unobserve(currentDomRef)
+  }, [])
+
+  return (
+    <div
+      className={`${styles.fadeIntoView} ${isVisible ? styles.isVisible : ''} ${
+        alreadyViewed ? styles.alreadyViewed : ''
+      }`}
+      ref={domRef}
+    >
+      {children}
+    </div>
+  )
+}
+
+export default FadeIntoView
--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -1,13 +1,17 @@
 import ProjectCard from './ProjectCard'
 import projectsData from '@/data/projects.json'
 
+import FadeIntoView from '@/app/animations/FadeIntoView'
+
 export default function Projects() {
   return (
     <>
       <div className='flex w-full flex-row flex-wrap justify-center gap-8 pt-16'>
         {projectsData.projects.map((project, index) => (
           <div key={index}>
-            <ProjectCard project={project} />
+            <FadeIntoView>
+              <ProjectCard project={project} />
+            </FadeIntoView>
           </div>
         ))}
       </div>
