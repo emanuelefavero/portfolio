2646390d42cca0133fb4e1679474d2c1c4508fc2
emanuelefavero <emafaux89@gmail.com>
2023-05-29 10:11:10 +0200

    add - hide and show header on scroll

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -1,9 +1,42 @@
+'use client'
+
+import { useEffect, useState } from 'react'
 import HeaderLogo from './HeaderLogo'
 import HeaderMenu from './HeaderMenu'
 
 export default function Header() {
+  const [prevScrollPos, setPrevScrollPos] = useState(0)
+  const [isHeaderHidden, setIsHeaderHidden] = useState(false)
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollPos = window.pageYOffset
+
+      if (prevScrollPos > currentScrollPos) {
+        setIsHeaderHidden(false)
+      } else {
+        setIsHeaderHidden(true)
+      }
+
+      setPrevScrollPos(currentScrollPos)
+    }
+
+    window.addEventListener('scroll', handleScroll)
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [prevScrollPos])
+
   return (
-    <div className='flex w-full items-center justify-between bg-slate-200 px-2 py-3'>
+    <div
+      className={`fixed ${
+        isHeaderHidden ? 'opacity-0' : 'opacity-100'
+      } z-50 flex w-full items-center justify-between bg-slate-200/75 px-2 py-3 backdrop-blur-lg backdrop-filter transition-all duration-200`}
+      style={{
+        transform: isHeaderHidden ? 'translateY(-100%)' : 'translateY(0)',
+      }}
+    >
       <HeaderLogo />
       <HeaderMenu />
     </div>
