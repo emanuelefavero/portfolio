2aa7d8b38c7dca5420e54d440edab59428a44497
emanuelefavero <emafaux89@gmail.com>
2023-05-31 13:35:50 +0200

    add fade in slide up slow animation to hamburger menu

--- app/components/HamburgerMenu.tsx
+++ app/components/HamburgerMenu.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import styles from '@/app/styles/animations.module.scss'
 import HamburgerMenuButton from './HamburgerMenuButton'
 import HeaderMenuItems from './HeaderMenuItems'
 import { useState } from 'react'
@@ -15,21 +16,23 @@ export default function HamburgerMenu() {
       />
 
       <ul
-        className={`${
-          isHamburgerMenuOpen ? 'open' : 'closed'
-        } text-slate text-slate absolute right-0 top-0 flex flex-col items-center justify-center border-2 border-b-slate-700 bg-slate-300/95 text-2xl font-semibold text-slate-700 transition-all duration-200`}
+        className={`text-slate text-slate absolute right-0 top-0 flex flex-col items-center justify-center border-2 border-b-slate-700 bg-slate-300/95 text-2xl font-semibold text-slate-700 transition-all duration-200`}
         style={{
           width: isHamburgerMenuOpen ? '100%' : '0',
           height: isHamburgerMenuOpen ? '100vh' : '0',
           opacity: isHamburgerMenuOpen ? '1' : '0',
           overflow: 'hidden',
-          // position: 'fixed',
+          position: 'fixed',
           transformOrigin: 'top right',
           transitionProperty: 'width, height, opacity',
           transition: 'all 0.2s ease-in-out',
         }}
       >
-        <div className='flex flex-col gap-8'>
+        <div
+          className={`flex flex-col gap-8 ${
+            isHamburgerMenuOpen ? styles.fadeInSlideUpSlow : ''
+          }`}
+        >
           <HeaderMenuItems setIsHamburgerMenuOpen={setIsHamburgerMenuOpen} />
         </div>
       </ul>
