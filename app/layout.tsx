d96b8e7273e3e0122371a82e704861da21b5f204
emanuelefavero <emafaux89@gmail.com>
2023-06-02 22:46:41 +0200

    add metadata in layout.tsx, robots and sitemap

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,12 +8,64 @@ import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
+// * --- METADATA ---
 export const metadata = {
   title: 'Emanuele Favero Portfolio',
   description:
     'Based in Italy, Emanuele Favero is a software engineer passionate about coding UX/UI experiences for the web',
-}
 
+  // Open Graph
+  openGraph: {
+    title: 'Emanuele Favero Portfolio',
+    description: 'A software engineer based in Italy',
+    url: 'https://emanuelefavero.com',
+    siteName: 'Emanuele Favero Portfolio',
+    images: [
+      {
+        url: 'https://emanuelefavero.com/icon-512x512.png',
+        width: 1200,
+        height: 630,
+        alt: 'Emanuele Favero Portfolio',
+      },
+    ],
+    locale: 'it_IT',
+    type: 'website',
+  },
+
+  // robots
+  robots: {
+    index: true,
+    follow: true,
+  },
+
+  // icons
+  icon: {
+    icon: '/icon-512x512.png',
+    shortcut: '/icon-192x192.png',
+    apple: '/icon-192x192.png',
+  },
+
+  // themeColor
+  themeColor: '#4f46e5',
+
+  // manifest
+  manifest: 'https://emanuelefavero.com/manifest.json',
+
+  // TODO: Twitter
+
+  // viewport
+  viewport: {
+    width: 'device-width',
+    initialScale: 1,
+    // maximumScale: 1,
+  },
+
+  // more metadata options
+  // @see https://nextjs.org/docs/app/api-reference/functions/generate-metadata
+}
+// --- ---- ---
+
+// * --- LAYOUT ---
 export default function RootLayout({
   children,
 }: {
