822cc1a74682302d649e498b5cc95533ea4cbc1f
emanuelefavero <emafaux89@gmail.com>
2023-06-02 20:03:36 +0200

    fix 404 pdf file error on load

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+  // Prevent 404 error on public folder assets (like pdf files)
+  assetPrefix: isProd ? 'https://emanuelefavero.com' : '',
+}
 
 module.exports = nextConfig
