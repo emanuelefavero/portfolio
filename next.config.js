eac16ecc5662e9dfe10b5d6f0022a94fd9423fd0
emanuelefavero <emafaux89@gmail.com>
2023-06-02 20:06:07 +0200

    remove assetPrefix in next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,5 @@
 /** @type {import('next').NextConfig} */
 
-const isProd = process.env.NODE_ENV === 'production'
-
-const nextConfig = {
-  // Prevent 404 error on public folder assets (like pdf files)
-  assetPrefix: isProd ? 'https://emanuelefavero.com' : '',
-}
+const nextConfig = {}
 
 module.exports = nextConfig
