469eeba2a8da1173659f64c4df5201705af19d83
emanuelefavero <emafaux89@gmail.com>
2023-06-02 11:39:24 +0200

    hide skills code part on screens less than 240px wide

--- app/components/SkillsCode.tsx
+++ app/components/SkillsCode.tsx
@@ -3,7 +3,7 @@ import TypewriterTextEffect from './TypewriterTextEffect'
 export default function SkillsCode() {
   return (
     <>
-      <div className='mb-20 min-w-[154px] select-none rounded-md border border-[#103453] bg-[#154269] px-2 py-[0.1rem] font-semibold text-[#d2dbe8] shadow-sm shadow-[#154269]/40'>
+      <div className='mb-20 hidden min-w-[154px] select-none rounded-md border border-[#103453] bg-[#154269] px-2 py-[0.1rem] font-semibold text-[#d2dbe8] shadow-sm shadow-[#154269]/40 3xs:block'>
         <span className='mr-3'>1</span>
 
         {/* TYPEWRITER TEXT EFFECT */}
