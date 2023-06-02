8b8d6daf5142e01e2c066c1dd5766122349873c2
emanuelefavero <emafaux89@gmail.com>
2023-06-02 19:56:43 +0200

    improve accessibility - remove type='button' to a tag

--- app/components/HeroButton.tsx
+++ app/components/HeroButton.tsx
@@ -6,7 +6,6 @@ export default function HeroButton() {
     <>
       <Link
         href='/#contact-me'
-        type='button'
         className={`${styles.HeroButton} hover:tracking-wide' mb-16 max-w-max select-none rounded-full bg-slate-900 px-2 py-2 text-sm font-bold text-white shadow-sm shadow-indigo-300 3xs:px-6 3xs:text-xl 2xs:mb-24 xs:mb-36`}
       >
         Let&apos;s Talk
