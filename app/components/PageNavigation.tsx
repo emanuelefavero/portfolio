1a8d157934946aa316b0b4a5b0455261ed9c628a
emanuelefavero <emafaux89@gmail.com>
2023-06-02 12:14:56 +0200

    hide page navigation on screens smaller than 240px

--- app/components/PageNavigation.tsx
+++ app/components/PageNavigation.tsx
@@ -9,7 +9,7 @@ export default function PageNavigation() {
   const activeSection = useActiveSection()
 
   return (
-    <nav className='fixed right-0 top-0 z-40 flex h-screen w-[4.2rem] select-none flex-col items-center justify-center gap-4'>
+    <nav className='fixed right-0 top-0 z-40 hidden h-screen w-[4.2rem] select-none flex-col items-center justify-center gap-4 3xs:flex'>
       {/* Home */}
       <ScrollLink
         title='Home'
